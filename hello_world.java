5afeccf619d549e1fcdb213cf99906c29983aee5
samnikhil7892 <55790509+samnikhil7892@users.noreply.github.com>
2019-10-23 15:15:54 +0530

    Update Hello_World
    Changes made through Git Hub

--- hello_world.java
+++ hello_world.java
@@ -1,3 +1,11 @@
-total 0
--rw-r--r-- 1 spuvvada 1049089 0 Oct 23 14:56 hello_world.java 
-Hello world programming to be edited in java
+package helloworld;
+
+public class HelloWorld {
+
+	public static void main(String args[]){
+
+		System.out.println("Hello World, Maven");
+
+	}
+
+}
